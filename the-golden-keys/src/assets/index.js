447b63672386466c8b656339e26fda3d07781649
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2024-04-11 12:18:12 +0100

    Created and styled responsive Header

--- the-golden-keys/src/assets/index.js
+++ the-golden-keys/src/assets/index.js
@@ -1,11 +1,14 @@
-import goldenKeysLogo from './goldenKeysLogo.svg';
+import headerPhoneIcon from './header-phone-icon.svg';
 
-import facebookIcon from './facebookIcon.svg';
-import phoneIcon from './phoneIcon.svg';
-import mobileIcon from './mobileIcon.svg';
-import emailIcon from './emailIcon.svg';
+import goldenKeysLogo from './golden-keys-logo.svg';
+
+import facebookIcon from './social-media-icons/facebook-icon.svg';
+import phoneIcon from './social-media-icons/phone-icon.svg';
+import mobileIcon from './social-media-icons/mobile-icon.svg';
+import emailIcon from './social-media-icons/email-icon.svg';
 
 export {
+    headerPhoneIcon,
     goldenKeysLogo,
     facebookIcon,
     phoneIcon,
--- the-golden-keys/src/components/common/header/Header.jsx
+++ the-golden-keys/src/components/common/header/Header.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { headerPhoneIcon } from '../../../assets';
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <header className="fixed w-full h-24 lg:h-12 top-0 left-0 z-50 bg-red text-[#fff] text-xl md:text-2xl xl:text-3xl">
+      <div className="flex flex-col lg:flex-row items-center justify-center mx-auto h-full gap-2 font-bold">
+        <div className="flex flex-row justify-center items-center">
+          <p className="text-center">
+            ΚΛΕΙΔΑΡΑΣ ΑΜΠΕΛΟΚΗΠΟΙ ΘΕΣΣΑΛΟΝΙΚΗΣ
+          </p>
+        </div>
+        <div className="flex flex-row items-center justify-center gap-2">
+          <img src={headerPhoneIcon} alt="phone icon" className="h-6 xl:h-8" />
+          <p>
+            2310 78.77.77 - 6987 78.77.77
+          </p>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
