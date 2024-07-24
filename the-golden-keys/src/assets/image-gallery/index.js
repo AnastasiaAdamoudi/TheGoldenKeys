c602bc8e8916ab1130f170e79984a9fca339ce75
AnastasiaAdamoudi <anastasiaadamoudi@gmail.com>
2024-07-24 22:21:30 +0100

    Added photos to photo gallery page

--- the-golden-keys/src/assets/image-gallery/index.js
+++ the-golden-keys/src/assets/image-gallery/index.js
@@ -60,6 +60,129 @@ import image54 from './gallery-page/54.jpg';
 import image55 from './gallery-page/55.jpg';
 import image56 from './gallery-page/56.jpg';
 import image57 from './gallery-page/57.jpg';
+import image58 from './gallery-page/58.jpeg';
+import image59 from './gallery-page/59.jpeg';
+import image60 from './gallery-page/60.jpeg';
+import image61 from './gallery-page/61.jpeg';
+import image62 from './gallery-page/62.jpeg';
+import image63 from './gallery-page/63.jpeg';
+import image64 from './gallery-page/64.jpeg';
+import image65 from './gallery-page/65.jpeg';
+import image66 from './gallery-page/66.jpeg';
+import image67 from './gallery-page/67.jpeg';
+import image68 from './gallery-page/68.jpeg';
+import image69 from './gallery-page/69.jpeg';
+import image70 from './gallery-page/70.jpeg';
+import image71 from './gallery-page/71.jpeg';
+import image72 from './gallery-page/72.jpeg';
+import image73 from './gallery-page/73.jpeg';
+import image74 from './gallery-page/74.jpeg';
+import image75 from './gallery-page/75.jpeg';
+import image76 from './gallery-page/76.jpeg';
+import image77 from './gallery-page/77.jpeg';
+import image78 from './gallery-page/78.jpeg';
+import image79 from './gallery-page/79.jpeg';
+import image80 from './gallery-page/80.jpeg';
+import image81 from './gallery-page/81.jpeg';
+import image82 from './gallery-page/82.jpeg';
+import image83 from './gallery-page/83.jpeg';
+import image84 from './gallery-page/84.jpeg';
+import image85 from './gallery-page/85.jpeg';
+import image86 from './gallery-page/86.jpeg';
+import image87 from './gallery-page/87.jpeg';
+import image88 from './gallery-page/88.jpeg';
+import image89 from './gallery-page/89.jpeg';
+import image90 from './gallery-page/90.jpeg';
+import image91 from './gallery-page/91.jpeg';
+import image92 from './gallery-page/92.jpeg';
+import image93 from './gallery-page/93.jpeg';
+import image94 from './gallery-page/94.jpeg';
+import image95 from './gallery-page/95.jpeg';
+import image96 from './gallery-page/96.jpeg';
+import image97 from './gallery-page/97.jpeg';
+import image98 from './gallery-page/98.jpeg';
+import image99 from './gallery-page/99.jpeg';
+import image100 from './gallery-page/100.jpeg';
+import image101 from './gallery-page/101.jpeg';
+import image102 from './gallery-page/102.jpeg';
+import image103 from './gallery-page/103.jpeg';
+import image104 from './gallery-page/104.jpeg';
+import image105 from './gallery-page/105.jpeg';
+import image106 from './gallery-page/106.jpeg';
+import image107 from './gallery-page/107.jpeg';
+import image108 from './gallery-page/108.jpeg';
+import image109 from './gallery-page/109.jpeg';
+import image110 from './gallery-page/110.jpeg';
+import image111 from './gallery-page/111.jpeg';
+import image112 from './gallery-page/112.jpeg';
+import image113 from './gallery-page/113.jpeg';
+import image114 from './gallery-page/114.jpeg';
+import image115 from './gallery-page/115.jpeg';
+import image116 from './gallery-page/116.jpeg';
+import image117 from './gallery-page/117.jpeg';
+import image118 from './gallery-page/118.jpeg';
+import image119 from './gallery-page/119.jpeg';
+import image120 from './gallery-page/120.jpeg';
+import image121 from './gallery-page/121.jpeg';
+import image122 from './gallery-page/122.jpeg';
+import image123 from './gallery-page/123.jpeg';
+import image124 from './gallery-page/124.jpeg';
+import image125 from './gallery-page/125.jpeg';
+import image126 from './gallery-page/126.jpeg';
+import image127 from './gallery-page/127.jpeg';
+import image128 from './gallery-page/128.jpeg';
+import image129 from './gallery-page/129.jpeg';
+import image130 from './gallery-page/130.jpeg';
+import image131 from './gallery-page/131.jpeg';
+import image132 from './gallery-page/132.jpeg';
+import image133 from './gallery-page/133.jpeg';
+import image134 from './gallery-page/134.jpeg';
+import image135 from './gallery-page/135.jpeg';
+import image136 from './gallery-page/136.jpeg';
+import image137 from './gallery-page/137.jpeg';
+import image138 from './gallery-page/138.jpeg';
+import image139 from './gallery-page/139.jpeg';
+import image140 from './gallery-page/140.jpeg';
+import image141 from './gallery-page/141.jpeg';
+import image142 from './gallery-page/142.jpeg';
+import image143 from './gallery-page/143.jpeg';
+import image144 from './gallery-page/144.jpeg';
+import image145 from './gallery-page/145.jpeg';
+import image146 from './gallery-page/146.jpeg';
+import image147 from './gallery-page/147.jpeg';
+import image148 from './gallery-page/148.jpeg';
+import image149 from './gallery-page/149.jpeg';
+import image150 from './gallery-page/150.jpeg';
+import image151 from './gallery-page/151.jpeg';
+import image152 from './gallery-page/152.jpeg';
+import image153 from './gallery-page/153.jpeg';
+import image154 from './gallery-page/154.jpeg';
+import image155 from './gallery-page/155.jpeg';
+import image156 from './gallery-page/156.jpeg';
+import image157 from './gallery-page/157.jpeg';
+import image158 from './gallery-page/158.jpeg';
+import image159 from './gallery-page/159.jpeg';
+import image160 from './gallery-page/160.jpeg';
+import image161 from './gallery-page/161.jpeg';
+import image162 from './gallery-page/162.jpeg';
+import image163 from './gallery-page/163.jpeg';
+import image164 from './gallery-page/164.jpeg';
+import image165 from './gallery-page/165.jpeg';
+import image166 from './gallery-page/166.jpeg';
+import image167 from './gallery-page/167.jpeg';
+import image168 from './gallery-page/168.jpeg';
+import image169 from './gallery-page/169.jpeg';
+import image170 from './gallery-page/170.jpeg';
+import image171 from './gallery-page/171.jpeg';
+import image172 from './gallery-page/172.jpeg';
+import image173 from './gallery-page/173.jpeg';
+import image174 from './gallery-page/174.jpeg';
+import image175 from './gallery-page/175.jpeg';
+import image176 from './gallery-page/176.jpeg';
+import image177 from './gallery-page/177.jpeg';
+import image178 from './gallery-page/178.jpeg';
+import image179 from './gallery-page/179.jpeg';
+import image180 from './gallery-page/180.jpeg';
 
 export const photos = [
     {
@@ -369,5 +492,620 @@ export const galleryImages = [
         id: 57,
         src: image57,
         alt: 'Εικόνα 57',
+    },
+    {
+        id: 58,
+        src: image58,
+        alt: 'Εικόνα 58',
+    },
+    {
+        id: 59,
+        src: image59,
+        alt: 'Εικόνα 59',
+    },
+    {
+        id: 60,
+        src: image60,
+        alt: 'Εικόνα 60',
+    },
+    {
+        id: 61,
+        src: image61,
+        alt: 'Εικόνα 61',
+    },
+    {
+        id: 62,
+        src: image62,
+        alt: 'Εικόνα 62',
+    },
+    {
+        id: 63,
+        src: image63,
+        alt: 'Εικόνα 63',
+    },
+    {
+        id: 64,
+        src: image64,
+        alt: 'Εικόνα 64',
+    },
+    {
+        id: 65,
+        src: image65,
+        alt: 'Εικόνα 65',
+    },
+    {
+        id: 66,
+        src: image66,
+        alt: 'Εικόνα 66',
+    },
+    {
+        id: 67,
+        src: image67,
+        alt: 'Εικόνα 67',
+    },
+    {
+        id: 68,
+        src: image68,
+        alt: 'Εικόνα 68',
+    },
+    {
+        id: 69,
+        src: image69,
+        alt: 'Εικόνα 69',
+    },
+    {
+        id: 70,
+        src: image70,
+        alt: 'Εικόνα 70',
+    },
+    {
+        id: 71,
+        src: image71,
+        alt: 'Εικόνα 71',
+    },
+    {
+        id: 72,
+        src: image72,
+        alt: 'Εικόνα 72',
+    },
+    {
+        id: 73,
+        src: image73,
+        alt: 'Εικόνα 73',
+    },
+    {
+        id: 74,
+        src: image74,
+        alt: 'Εικόνα 74',
+    },
+    {
+        id: 75,
+        src: image75,
+        alt: 'Εικόνα 75',
+    },
+    {
+        id: 76,
+        src: image76,
+        alt: 'Εικόνα 76',
+    },
+    {
+        id: 77,
+        src: image77,
+        alt: 'Εικόνα 77',
+    },
+    {
+        id: 78,
+        src: image78,
+        alt: 'Εικόνα 78',
+    },
+    {
+        id: 79,
+        src: image79,
+        alt: 'Εικόνα 79',
+    },
+    {
+        id: 80,
+        src: image80,
+        alt: 'Εικόνα 80',
+    },
+    {
+        id: 81,
+        src: image81,
+        alt: 'Εικόνα 81',
+    },
+    {
+        id: 82,
+        src: image82,
+        alt: 'Εικόνα 82',
+    },
+    {
+        id: 83,
+        src: image83,
+        alt: 'Εικόνα 83',
+    },
+    {
+        id: 84,
+        src: image84,
+        alt: 'Εικόνα 84',
+    },
+    {
+        id: 85,
+        src: image85,
+        alt: 'Εικόνα 85',
+    },
+    {
+        id: 86,
+        src: image86,
+        alt: 'Εικόνα 86',
+    },
+    {
+        id: 87,
+        src: image87,
+        alt: 'Εικόνα 87',
+    },
+    {
+        id: 88,
+        src: image88,
+        alt: 'Εικόνα 88',
+    },
+    {
+        id: 89,
+        src: image89,
+        alt: 'Εικόνα 89',
+    },
+    {
+        id: 90,
+        src: image90,
+        alt: 'Εικόνα 90',
+    },
+    {
+        id: 91,
+        src: image91,
+        alt: 'Εικόνα 91',
+    },
+    {
+        id: 92,
+        src: image92,
+        alt: 'Εικόνα 92',
+    },
+    {
+        id: 93,
+        src: image93,
+        alt: 'Εικόνα 93',
+    },
+    {
+        id: 94,
+        src: image94,
+        alt: 'Εικόνα 94',
+    },
+    {
+        id: 95,
+        src: image95,
+        alt: 'Εικόνα 95',
+    },
+    {
+        id: 96,
+        src: image96,
+        alt: 'Εικόνα 96',
+    },
+    {
+        id: 97,
+        src: image97,
+        alt: 'Εικόνα 97',
+    },
+    {
+        id: 98,
+        src: image98,
+        alt: 'Εικόνα 98',
+    },
+    {
+        id: 99,
+        src: image99,
+        alt: 'Εικόνα 99',
+    },
+    {
+        id: 100,
+        src: image100,
+        alt: 'Εικόνα 100',
+    },
+    {
+        id: 101,
+        src: image101,
+        alt: 'Εικόνα 101',
+    },
+    {
+        id: 102,
+        src: image102,
+        alt: 'Εικόνα 102',
+    },
+    {
+        id: 103,
+        src: image103,
+        alt: 'Εικόνα 103',
+    },
+    {
+        id: 104,
+        src: image104,
+        alt: 'Εικόνα 104',
+    },
+    {
+        id: 105,
+        src: image105,
+        alt: 'Εικόνα 105',
+    },
+    {
+        id: 106,
+        src: image106,
+        alt: 'Εικόνα 106',
+    },
+    {
+        id: 107,
+        src: image107,
+        alt: 'Εικόνα 107',
+    },
+    {
+        id: 108,
+        src: image108,
+        alt: 'Εικόνα 108',
+    },
+    {
+        id: 109,
+        src: image109,
+        alt: 'Εικόνα 109',
+    },
+    {
+        id: 110,
+        src: image110,
+        alt: 'Εικόνα 110',
+    },
+    {
+        id: 111,
+        src: image111,
+        alt: 'Εικόνα 111',
+    },
+    {
+        id: 112,
+        src: image112,
+        alt: 'Εικόνα 112',
+    },
+    {
+        id: 113,
+        src: image113,
+        alt: 'Εικόνα 113',
+    },
+    {
+        id: 114,
+        src: image114,
+        alt: 'Εικόνα 114',
+    },
+    {
+        id: 115,
+        src: image115,
+        alt: 'Εικόνα 115',
+    },
+    {
+        id: 116,
+        src: image116,
+        alt: 'Εικόνα 116',
+    },
+    {
+        id: 117,
+        src: image117,
+        alt: 'Εικόνα 117',
+    },
+    {
+        id: 118,
+        src: image118,
+        alt: 'Εικόνα 118',
+    },
+    {
+        id: 119,
+        src: image119,
+        alt: 'Εικόνα 119',
+    },
+    {
+        id: 120,
+        src: image120,
+        alt: 'Εικόνα 120',
+    },
+    {
+        id: 121,
+        src: image121,
+        alt: 'Εικόνα 121',
+    },
+    {
+        id: 122,
+        src: image122,
+        alt: 'Εικόνα 122',
+    },
+    {
+        id: 123,
+        src: image123,
+        alt: 'Εικόνα 123',
+    },
+    {
+        id: 124,
+        src: image124,
+        alt: 'Εικόνα 124',
+    },
+    {
+        id: 125,
+        src: image125,
+        alt: 'Εικόνα 125',
+    },
+    {
+        id: 126,
+        src: image126,
+        alt: 'Εικόνα 126',
+    },
+    {
+        id: 127,
+        src: image127,
+        alt: 'Εικόνα 127',
+    },
+    {
+        id: 128,
+        src: image128,
+        alt: 'Εικόνα 128',
+    },
+    {
+        id: 129,
+        src: image129,
+        alt: 'Εικόνα 129',
+    },
+    {
+        id: 130,
+        src: image130,
+        alt: 'Εικόνα 130',
+    },
+    {
+        id: 131,
+        src: image131,
+        alt: 'Εικόνα 131',
+    },
+    {
+        id: 132,
+        src: image132,
+        alt: 'Εικόνα 132',
+    },
+    {
+        id: 133,
+        src: image133,
+        alt: 'Εικόνα 133',
+    },
+    {
+        id: 134,
+        src: image134,
+        alt: 'Εικόνα 134',
+    },
+    {
+        id: 135,
+        src: image135,
+        alt: 'Εικόνα 135',
+    },
+    {
+        id: 136,
+        src: image136,
+        alt: 'Εικόνα 136',
+    },
+    {
+        id: 137,
+        src: image137,
+        alt: 'Εικόνα 137',
+    },
+    {
+        id: 138,
+        src: image138,
+        alt: 'Εικόνα 138',
+    },
+    {
+        id: 139,
+        src: image139,
+        alt: 'Εικόνα 139',
+    },
+    {
+        id: 140,
+        src: image140,
+        alt: 'Εικόνα 140',
+    },
+    {
+        id: 141,
+        src: image141,
+        alt: 'Εικόνα 141',
+    },
+    {
+        id: 142,
+        src: image142,
+        alt: 'Εικόνα 142',
+    },
+    {
+        id: 143,
+        src: image143,
+        alt: 'Εικόνα 143',
+    },
+    {
+        id: 144,
+        src: image144,
+        alt: 'Εικόνα 144',
+    },
+    {
+        id: 145,
+        src: image145,
+        alt: 'Εικόνα 145',
+    },
+    {
+        id: 146,
+        src: image146,
+        alt: 'Εικόνα 146',
+    },
+    {
+        id: 147,
+        src: image147,
+        alt: 'Εικόνα 147',
+    },
+    {
+        id: 148,
+        src: image148,
+        alt: 'Εικόνα 148',
+    },
+    {
+        id: 149,
+        src: image149,
+        alt: 'Εικόνα 149',
+    },
+    {
+        id: 150,
+        src: image150,
+        alt: 'Εικόνα 150',
+    },
+    {
+        id: 151,
+        src: image151,
+        alt: 'Εικόνα 151',
+    },
+    {
+        id: 152,
+        src: image152,
+        alt: 'Εικόνα 152',
+    },
+    {
+        id: 153,
+        src: image153,
+        alt: 'Εικόνα 153',
+    },
+    {
+        id: 154,
+        src: image154,
+        alt: 'Εικόνα 154',
+    },
+    {
+        id: 155,
+        src: image155,
+        alt: 'Εικόνα 155',
+    },
+    {
+        id: 156,
+        src: image156,
+        alt: 'Εικόνα 156',
+    },
+    {
+        id: 157,
+        src: image157,
+        alt: 'Εικόνα 157',
+    },
+    {
+        id: 158,
+        src: image158,
+        alt: 'Εικόνα 158',
+    },
+    {
+        id: 159,
+        src: image159,
+        alt: 'Εικόνα 159',
+    },
+    {
+        id: 160,
+        src: image160,
+        alt: 'Εικόνα 160',
+    },
+    {
+        id: 161,
+        src: image161,
+        alt: 'Εικόνα 161',
+    },
+    {
+        id: 162,
+        src: image162,
+        alt: 'Εικόνα 162',
+    },
+    {
+        id: 163,
+        src: image163,
+        alt: 'Εικόνα 163',
+    },
+    {
+        id: 164,
+        src: image164,
+        alt: 'Εικόνα 164',
+    },
+    {
+        id: 165,
+        src: image165,
+        alt: 'Εικόνα 165',
+    },
+    {
+        id: 166,
+        src: image166,
+        alt: 'Εικόνα 166',
+    },
+    {
+        id: 167,
+        src: image167,
+        alt: 'Εικόνα 167',
+    },
+    {
+        id: 168,
+        src: image168,
+        alt: 'Εικόνα 168',
+    },
+    {
+        id: 169,
+        src: image169,
+        alt: 'Εικόνα 169',
+    },
+    {
+        id: 170,
+        src: image170,
+        alt: 'Εικόνα 170',
+    },
+    {
+        id: 171,
+        src: image171,
+        alt: 'Εικόνα 171',
+    },
+    {
+        id: 172,
+        src: image172,
+        alt: 'Εικόνα 172',
+    },
+    {
+        id: 173,
+        src: image173,
+        alt: 'Εικόνα 173',
+    },
+    {
+        id: 174,
+        src: image174,
+        alt: 'Εικόνα 174',
+    },
+    {
+        id: 175,
+        src: image175,
+        alt: 'Εικόνα 175',
+    },
+    {
+        id: 176,
+        src: image176,
+        alt: 'Εικόνα 176',
+    },
+    {
+        id: 177,
+        src: image177,
+        alt: 'Εικόνα 177',
+    },
+    {
+        id: 178,
+        src: image178,
+        alt: 'Εικόνα 178',
+    },
+    {
+        id: 179,
+        src: image179,
+        alt: 'Εικόνα 179',
+    },
+    {
+        id: 180,
+        src: image180,
+        alt: 'Εικόνα 180',
     },
 ];
